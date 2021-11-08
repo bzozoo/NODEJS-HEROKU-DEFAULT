a20c92261ebb8b8eebaf2a9cf2b5fc28ad6a9422
Bata Zoltán <47896536+bzozoo@users.noreply.github.com>
2021-11-08 16:57:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+const welcomeString = '<h1>Hello World</h1>' + process.env.ownvar;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>' + process.env.ownvar);
+  res.end(welcomeString);
 });
 
 server.listen(port,() => {
